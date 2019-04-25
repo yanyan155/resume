411fa232bf8c72fa8c412b4063d0513fd8de8ab4
yanyan155 <jm@tut.by>
2019-04-25 18:13:07 +0300

    feat: add neuton mail to projects

---
+++ projects/neutron-mail/js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+  new WOW().init();
+  
+  $('.slider-clients').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    dots: false,
+    arrows: false,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+  $('.slider-partners').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    dots: false,
+    arrows: false,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+
+  let blockquotesControl = document.querySelector('.quote-controls');
+  blockquotesControl.addEventListener('click', (event) => {
+    let target = event.target;
+    if(target.dataset.quote 
+       && !target.classList.contains('active') ) {
+
+      let number = Number(target.dataset.quote);
+      let allLi = blockquotesControl.querySelectorAll("li");
+      delSetActiveClass(allLi, number);
+      
+      let allquotes = document.querySelectorAll(".quotes-wrap .quote-block");
+      delSetActiveClass(allquotes , number);
+    }
+  })
+  function delSetActiveClass(elems, number) {
+    let array = Array.from(elems);
+    array.forEach((elem, i, arr) => {
+      elem.classList.contains('active') ? elem.classList.remove("active") : '';
+      number === i+1 ? elem.classList.add("active") : '';
+    }); 
+  }
+});
